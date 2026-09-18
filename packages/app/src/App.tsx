a4161b01770eb9c974ec4d998c0c5968ce4360c3
johnfercher <johnathanfercher22@gmail.com>
2026-09-18 14:06:56 -0300

    fix chart graph

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,9 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import catalogGraphPlugin from '@backstage/plugin-catalog-graph/alpha';
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 
 export default createApp({
-  features: [catalogPlugin, navModule, homeModule],
+  features: [catalogPlugin, catalogGraphPlugin, navModule, homeModule],
 });
